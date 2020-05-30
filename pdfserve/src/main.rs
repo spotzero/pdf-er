5605bea5bbe0d263d876dd6850e3421ea60b892a
David Pascoe-Deslauriers <dpascoed@coldfrontlabs.ca>
2020-05-29 22:59:42 -0400

    firefox crashes when generating more than one pdf, so for now it's locked to one at a time

--- pdfserve/src/main.rs
+++ pdfserve/src/main.rs
@@ -1,17 +1,15 @@
 #![feature(proc_macro_hygiene)]
 #![feature(decl_macro)]
 
-#[macro_use] extern crate rocket;
-
-#[cfg(test)] mod tests;
+#[macro_use]
+extern crate rocket;
 
 use rocket::http::ContentType;
 use rocket::http::Status;
 use rocket::request::Form;
-use rocket::response::Response;
 use rocket::response::status;
+use rocket::response::Response;
 use rocket::State;
-// use std::env;
 use std::fs;
 use std::io::Cursor;
 use std::process::Command;
@@ -29,12 +27,14 @@ struct RemoteURL {
 }
 
 #[get("/?<remoteurl..>")]
-fn getpdf(remoteurl: Option<Form<RemoteURL>>, screens: State<AvailableScreens>) -> Result<Response<'static>, status::Custom<String>> {
+fn getpdf(
+    remoteurl: Option<Form<RemoteURL>>,
+    screens: State<AvailableScreens>,
+) -> Result<Response<'static>, status::Custom<String>> {
     if let Some(remoteurl) = remoteurl {
-
         let screen = get_screen(&screens);
         if let Err(e) = screen {
-            return Err(status::Custom(Status::ServiceUnavailable, e.to_string()))
+            return Err(status::Custom(Status::TooManyRequests, e.to_string()));
         }
         let screen = screen.unwrap();
 
@@ -42,20 +42,20 @@ fn getpdf(remoteurl: Option<Form<RemoteURL>>, screens: State<AvailableScreens>) 
             let name = Uuid::new_v4().to_string();
             let pdf_path = format!("/output/{}.pdf", name);
             let pdf_status = Command::new("/app/bin/generate-pdf.sh")
-                .env("DEV", "1")
+                //.env("DEV", "1")
                 .env("URL", &remoteurl.url)
                 .env("PDFNAME", &name)
                 .status();
 
             if let Err(e) = pdf_status {
                 shutdown(name, screen, &screens);
-                return Err(status::Custom(Status::ServiceUnavailable, e.to_string()))
+                return Err(status::Custom(Status::ServiceUnavailable, e.to_string()));
             }
 
             let pdf_file = fs::read(pdf_path);
             if let Err(e) = pdf_file {
                 shutdown(name, screen, &screens);
-                return Err(status::Custom(Status::ServiceUnavailable, e.to_string()))
+                return Err(status::Custom(Status::ServiceUnavailable, e.to_string()));
             }
 
             let pdf_file = pdf_file.unwrap();
@@ -69,18 +69,22 @@ fn getpdf(remoteurl: Option<Form<RemoteURL>>, screens: State<AvailableScreens>) 
             Ok(response)
         } else {
             release_screen(screen, &screens);
-            Err(status::Custom(Status::NotAcceptable, "URL doesn't appear to be valid.".into()))
+            Err(status::Custom(
+                Status::NotAcceptable,
+                "URL doesn't appear to be valid.".into(),
+            ))
         }
     } else {
-        Err(status::Custom(Status::NotAcceptable, "You must specify a URL in the GET parameter to use this service (add: ?url=<URI>)".into()))
+        Err(status::Custom(
+            Status::NotAcceptable,
+            "You must specify a URL in the GET parameter to use this service (add: ?url=<URI>)"
+                .into(),
+        ))
     }
 }
 
 fn shutdown(name: String, screen: usize, screens: &AvailableScreens) {
-    let _status = Command::new("rm")
-        .arg("-Rf")
-        .arg(format!("/output/{}*", name))
-        .status();
+    let _ = fs::remove_file(format!("/output/{}.pdf", name));
     release_screen(screen, &screens);
 }
 
@@ -88,7 +92,7 @@ fn get_screen(screens: &AvailableScreens) -> Result<usize, String> {
     let mut lock = screens.screens.lock().expect("lock shared data");
     let mut screen = 0;
     let mut no_screens = true;
-    
+
     for (screen_id, used) in lock.iter().enumerate() {
         if !used {
             screen = screen_id;
@@ -97,10 +101,10 @@ fn get_screen(screens: &AvailableScreens) -> Result<usize, String> {
     }
 
     if no_screens {
-      Err("No available screens".to_string())
+        Err("No screens currently available, try again later.".to_string())
     } else {
-      lock[screen] = true;
-      Ok(screen)
+        lock[screen] = true;
+        Ok(screen)
     }
 }
 
@@ -112,6 +116,8 @@ fn release_screen(screen: usize, screens: &AvailableScreens) {
 fn main() {
     rocket::ignite()
         .mount("/", routes![getpdf])
-        .manage(AvailableScreens { screens: Mutex::new(vec![false;4])})
+        .manage(AvailableScreens {
+            screens: Mutex::new(vec![false; 1]),
+        })
         .launch();
 }
